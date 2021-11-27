85991138c769389aa20ca00727d8c845f36fb304
soda <79681269+flat-map@users.noreply.github.com>
2021-11-27 12:11:14 +0900

    update input.c

--- example/input.c
+++ example/input.c
@@ -1,41 +1,72 @@
 
+/* quicksort program */
+
 #include <stdio.h>
-  // 値の入れ替え
-  void swap(int*x
- ,int* y ) { int
-   temp;// 値を一時保存する変数
-  temp=  
-        * 
-        x  ; * x=    
-             *
- y    ;
-   * y=temp;}/***
-   * pivotを決め、
-   * 全データをpivotを境目に振り分け、
-   * pivotの添え字を返す
-   */int partition (  int  array [
-   ] ,  int left ,int right) {int i,   j  ,  pivot 
-  
-  ;i=left; j= right +1; pivot = left;  // 先頭要素をpivotとする
-do{do { i
-            ++   ; }while (  array [ i ] < array[pivot]  );do  
- {  j-- ; }  while (array [pivot]<array[ j
-]) ;  // pivotより小さいものを左へ、大きいものを右へ
-if( i<j){ swap( &  array[i], &
-           array[j] ); }
-   }while(i <j ) ; swap   (& 
-array[pivot ] ,    & array[j
-           ]   ); // pivotを更新
-return j  ;    }/* クイックソート */
-  void quick_sort (int array[],  int left,int right) {int pivot   
-;if (left<  right  ){ pivot=partition (array  ,left ,  right) ;quick_sort (array,left ,pivot-1); // pivotを境に再帰的にクイックソート
-quick_sort( array, pivot +   
-             1     ,right);} }   
-int main(void)   {int array [ 10  
-    ]   ={2,1, 8,   5  ,4, 7, 
- 9   ,  0, 6  ,3 };
-           int      i ;printf("Before sort: ");for(i=0; i <  10;i++ ) { printf ("%d ",   array[
-        i ] ) ;} printf("\n")
-  ; quick_sort(array,    0,9 
-           ) ; printf   (  "After sort: ");for(i=0; i<    10; i  ++){ printf( "%d ",array [ i])  ;  
- }printf ("\n") ;   return 0  ; }
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void init_rand( int* array , int length ){
+    int i;
+    for( i = 0; i < length; i++ ){
+        array[i] = rand() % 100 + 1;
+    }
+}
+
+void swap_int( int* a , int* b ){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quick_sort( int* array , int left , int right ){
+    int pivot;
+    int i;
+    int j;
+    int tmp;
+
+    if( left < right ){
+        // set pivot to center of array
+        pivot = array[ (left+right)/2 ];
+
+        i = left - 1;
+        j = right + 1;
+        while(1){
+            while( array[++i] < pivot );
+            while( array[--j] > pivot );
+            if( i >= j )    break;
+            swap_int( &array[i] , &array[j] );
+        }
+        quick_sort( array , left , i-1 );
+        quick_sort( array , j+1, right );
+    }
+}
+
+int main( int args , char** argv ){
+    
+    int array_length = atoi( argv[1] );
+    srand( (unsigned)time(NULL) );
+
+    int* array = (int*)malloc( sizeof(int) * array_length );
+    if( array == NULL ) return -1;
+
+    init_rand( array , array_length );
+
+    int i;
+
+    printf("Brefore\n");
+    for( i = 0; i < array_length; i++ ){
+        printf( "%3d," , array[i] );
+    }
+    printf("\n");
+
+    quick_sort( array , 0 , array_length - 1 );
+
+    printf("After\n");
+    for( i = 0; i < array_length; i++ )
+        printf( "%3d," , array[i] );
+    printf("\n");
+    
+    free(array);
+    return 0;
+}
